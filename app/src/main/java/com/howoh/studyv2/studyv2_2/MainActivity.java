946ee51346931eccac630153a89398e02c727154
howoh <hwdep001@gmail.com>
2017-11-05 14:16:52 +0900

    clean

--- app/src/main/java/com/howoh/studyv2/studyv2_2/MainActivity.java
+++ app/src/main/java/com/howoh/studyv2/studyv2_2/MainActivity.java
@@ -1,6 +1,5 @@
 package com.howoh.studyv2.studyv2_2;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
