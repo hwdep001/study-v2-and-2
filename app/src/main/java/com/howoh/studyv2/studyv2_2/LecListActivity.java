50332013bfc0b6b8ae470ce142459be8774a0408
howoh <hwdep001@gmail.com>
2017-12-04 15:55:54 +0900

    LecListActivity  - test setting 추가

--- app/src/main/java/com/howoh/studyv2/studyv2_2/LecListActivity.java
+++ app/src/main/java/com/howoh/studyv2/studyv2_2/LecListActivity.java
@@ -3,6 +3,8 @@ package com.howoh.studyv2.studyv2_2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -60,6 +62,28 @@ public class LecListActivity extends BaseActivity implements
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_test_setting) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
